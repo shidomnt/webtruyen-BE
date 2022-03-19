538f036b0e842f5549b94415c9362f37aa7c15e9
shidomnt <hatranquang02@gmail.com>
2022-03-19 22:52:58 +0700

    a

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import cors from 'cors';
 import * as db from './config/db';
 import routes from './routes';
 
-const PORT = 443;
+const PORT = process.env.PORT || 4000;
 
 const app = express();
 app.use(express.static(path.join(__dirname, '../public')));
